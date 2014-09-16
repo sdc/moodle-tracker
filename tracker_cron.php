4ef2835d8b332ade49b7504af439f2b53c9e5052
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2014-09-16 15:02:45 +0100

    Typo.

--- tracker_cron.php
+++ tracker_cron.php
@@ -10,7 +10,7 @@
  * @copyright 2014 Paul Vaughan
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
- * TODO:    Consider $thiscurse as an array, not an integer.
+ * TODO:    Consider $thiscourse as an array, not an integer.
  */
 
 // Script start time.
